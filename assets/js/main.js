f3f2f73bcfa0d809c91dc28e9030564075841f58
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-27 20:51:56 +0200

    Complete step-2

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,8 +22,27 @@ L'alert non si chiude in automatico. Dovete chiuderla voi a mano cliccando su ok
 
 // STEP 1
 /* crea un alert che mi mostra i 5 numeri */
-alert('Memorizza questi numeri' + ' ' + generateRandomNumbers());
+generateRandomNumbers();
 
+
+// STEP 2
+/* faccio partire un timer di 30 secondi */
+sec = setInterval(countDown, 1000);
+//imposto i secondi del timer
+seconds = 31
+
+
+// STEP 3 
+/* passati questi secondi chiedo all'utente i numeri da inserire */
+
+
+
+/*-------------------------------------------------------------------------- ---------------------*  funzioni create  *----------------------------------
+----------------------------------------------------------------------------*/
+
+
+//funzione step 1
+//funzione che genera un array con 5 numeri randomici al suo interno.
 function generateRandomNumbers() {
     //creo un array vuoto in cui inserire i numeri
     numberRandomArray = []
@@ -33,40 +52,36 @@ function generateRandomNumbers() {
         numRandom = Math.floor(Math.random() * 100)
         numberRandomArray.push(numRandom)
     }
+    alert('Memorizza questi numeri' + ' ' + numberRandomArray);
     return numberRandomArray
 }
 
-// STEP 2
-/* faccio partire un timer di 30 secondi */
-
-// STEP 3 
-/* passati questi secondi chiedo all'utente i numeri da inserire */
-
-
-
-/* placeToPrintNumber = document.querySelector('.num')
-let number;
-let numberUser;
-
-generaNumber()
-console.log(number);
-
-setTimeout(clearPage, 4000)
-sec = setInterval(countDown, 1000);
-seconds = 30
-
-setTimeout(function() {
-    numberUser = []
-    for (let index = 0; index < 5; index++) {
-        numUser = parseInt(prompt('write a number'))
-        placeToPrintNumber.innerHTML += numUser + ' '
-        numberUser.push(numUser)
+//funzione step 2
+function countDown() {
+    seconds--;
+    document.querySelector('.timer').innerHTML = seconds + 's'
+    if (seconds == 0) {
+        console.log('tempo scaduto fermati');
+        clearInterval(sec)
     }
-}, 300000)
+}
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+/* funzioni disponibili vecchia versione*/
+/* //funzione che genera randomicamente 5 numeri e li stampa a schermo
 function generaNumber() {
-    //faccio partire il cronometro
     number = []
     for (let index = 0; index < 5; index++) {
         num = Math.floor(Math.random() * 100)
@@ -75,19 +90,30 @@ function generaNumber() {
     }
     return number
 }
+ */
 
+
+
+/* //funzione che chiede all'utente di digitare 5 numeri mediante un prompt
+setTimeout(function() {
+    numberUser = []
+    for (let index = 0; index < 5; index++) {
+        numUser = parseInt(prompt('write a number'))
+        placeToPrintNumber.innerHTML += numUser + ' '
+        numberUser.push(numUser)
+    }
+}, 300000) */
+
+
+
+/* //funzione che pulisce la pagina
 function clearPage() {
     placeToPrintNumber.innerHTML = ''
-}
-
-function asknumberUser() {
-
-}
+} */
 
 
 
-
-
+/* //funzione che in base a come dichiariamo i secondi conta all'indietro fino a zero
 function countDown() {
     seconds--;
     document.querySelector('.timer').innerHTML = seconds + 's'
@@ -95,4 +121,6 @@ function countDown() {
         console.log('tempo scaduto fermati');
         clearInterval(sec)
     }
-} */
+}
+
+ */
