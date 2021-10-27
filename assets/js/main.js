856edf9ea5bb514f66c69bcc52e0c789458f56e0
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-27 14:39:43 +0200

    Complete structure folder

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+/* Descrizione:
+Un alert() espone 5 numeri generati casualmente.
+Da li parte un timer di 30 secondi.
+Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+Consigli del giorno:
+* Pensate prima in italiano.
+* Dividete in piccoli problemi la consegna.
+* Individuate gli elementi di cui avete bisogno per realizzare il programma.
+Info:
+L'alert non si chiude in automatico. Dovete chiuderla voi a mano cliccando su ok.
+ */
+
+
+/* Un alert() espone 5 numeri generati casualmente. */
