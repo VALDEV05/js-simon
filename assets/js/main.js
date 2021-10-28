5307c2e26e3f4acc4aadbee52cab42242366a62b
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-28 10:15:42 +0200

    Completed request, I removed the function that creates the timer. But I put it "inline" via setTimeout(function, milliseconds). Then the askNumber function generates me an array with the numbers written by the user, but at the same time also one for the correct answers. In that case it counts and writes the score on screen.

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,18 +23,13 @@ L'alert non si chiude in automatico. Dovete chiuderla voi a mano cliccando su ok
 // STEP 1
 /* crea un alert che mi mostra i 5 numeri */
 generateRandomNumbers();
-
+console.log('Il timer di 30 secondi è partito!!');
 
 // STEP 2
-/* faccio partire un timer di 30 secondi */
-sec = setInterval(countDown, 1000);
-//imposto i secondi del timer
-seconds = 31
-
-
-// STEP 3 
 /* passati questi secondi chiedo all'utente i numeri da inserire */
-
+//il timer lo setto da qui
+setTimeout(askNumberToUser, 30 * 1000);
+rightNumber = []
 
 
 /*-------------------------------------------------------------------------- ---------------------*  funzioni create  *----------------------------------
@@ -57,70 +52,27 @@ function generateRandomNumbers() {
 }
 
 //funzione step 2
-function countDown() {
-    seconds--;
-    document.querySelector('.timer').innerHTML = seconds + 's'
-    if (seconds == 0) {
-        console.log('tempo scaduto fermati');
-        clearInterval(sec)
-    }
-}
+//chiedi tramite il prompt 5 numeri all'utente
+//verififca se il numero inserito dall'utente è presente 
+//all'interno dell'array numberRandomArray è incluso quel numero
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* funzioni disponibili vecchia versione*/
-/* //funzione che genera randomicamente 5 numeri e li stampa a schermo
-function generaNumber() {
-    number = []
-    for (let index = 0; index < 5; index++) {
-        num = Math.floor(Math.random() * 100)
-        placeToPrintNumber.innerHTML += num + ' '
-        number.push(num)
-    }
-    return number
-}
- */
-
-
-
-/* //funzione che chiede all'utente di digitare 5 numeri mediante un prompt
-setTimeout(function() {
+function askNumberToUser() {
+    //array numeri utente
     numberUser = []
-    for (let index = 0; index < 5; index++) {
-        numUser = parseInt(prompt('write a number'))
-        placeToPrintNumber.innerHTML += numUser + ' '
-        numberUser.push(numUser)
+    while (numberUser.length < numberRandomArray.length) {
+        let userNum = parseInt(prompt('Vediamo se ti ricordi tutti i nunmeri!!!'));
+
+        // se i numeri inseriti dall'utente NON sono inclusi all'interno 
+        //dell'array allora pusho
+        if (!numberUser.includes(userNum)) {
+            numberUser.push(userNum);
+            //se il numero inserito dall'user è presente all'interno dell'array
+            //dei numeri randomici allora in quelcaso pusho
+            if (numberRandomArray.includes(userNum)) {
+                rightNumber.push(userNum)
+            }
+        }
     }
-}, 300000) */
-
-
-
-/* //funzione che pulisce la pagina
-function clearPage() {
-    placeToPrintNumber.innerHTML = ''
-} */
-
-
-
-/* //funzione che in base a come dichiariamo i secondi conta all'indietro fino a zero
-function countDown() {
-    seconds--;
-    document.querySelector('.timer').innerHTML = seconds + 's'
-    if (seconds == 0) {
-        console.log('tempo scaduto fermati');
-        clearInterval(sec)
-    }
+    //stampo il risultato a schermo
+    document.querySelector('.answer').innerHTML = `<h1>Hai indovinato questi numeri: ${[rightNumber]}, hai totalizzato un punteggio di: ${rightNumber.length}</h1>`
 }
-
- */
